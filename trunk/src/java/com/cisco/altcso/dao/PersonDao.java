d978b5decdaccfff1d23adbfcb6d98e7ded22164
mdshannan@gmail.com <mdshannan@gmail.com@c771589c-a823-0410-bf5b-c7253f783895>
2011-06-14 23:24:26 +0000

    dd

--- trunk/src/java/com/cisco/altcso/dao/PersonDao.java
+++
@@ -1,9 +0,0 @@
-package com.cisco.altcso.dao;
-
-import com.cisco.altcso.domain.Person;
-
-/**
- * DAO Interface for {@link Person}
- */
-public interface PersonDao extends GenericDao<Person, Long> {
-}
--- trunk/src/java/com/cisco/altcso/dao/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Database access object, or Repository.
- * @see http://en.wikipedia.org/wiki/Data_access_object
- */
-package com.cisco.altcso.dao;
